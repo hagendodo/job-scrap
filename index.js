b570b5f55c27595c728857f360ce3f15cab47b6e
ibnuwdodo <ibnuwdodo@gmail.com>
2024-05-19 11:04:07 +0700

    feat: add scrap

--- index.js
+++ index.js
@@ -1,41 +1,89 @@
 import { chromium } from "playwright";
-import { writeFile } from "fs";
+import { writeFile } from "fs/promises"; // Updated to use promises version of fs
 import { parse } from "node-html-parser";
+import { v4 } from "uuid";
 
 async function writeDataToFile(filename, data) {
   try {
-    await writeFile(filename, data);
-    console.log("Data has been written to file successfully.");
+    await writeFile(`output_jobs/${filename}.html`, data);
+    console.log("Status: Success");
+    console.log(
+      `Message: Data output_jobs/${filename}.html has been written to file successfully.`
+    );
   } catch (err) {
-    console.error("Error writing to file:", err);
+    throw new Error(err);
   }
 }
 
+function getRandomDelay(minDelay, maxDelay) {
+  // Generate a random delay between minDelay and maxDelay (inclusive)
+  return Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+}
+
 (async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+  const args = process.argv.slice(2); // Extracting command-line arguments excluding node and script name
+  if (args.length !== 1) {
+    console.error("Usage: npm run scrap <URL>");
+    process.exit(1);
+  }
+
+  const url = args[0];
+
+  const startTime = new Date(); // Record start time
+
+  const browser = await chromium.launch({ headless: true }); // Headful mode might reduce detection
+  const context = await browser.newContext({
+    viewport: { width: 1280, height: 800 },
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    javaScriptEnabled: true,
+  });
+
+  // Overriding the navigator properties to make the bot less detectable
+  await context.addInitScript(() => {
+    Object.defineProperty(navigator, "webdriver", {
+      get: () => false,
+    });
+    Object.defineProperty(navigator, "platform", {
+      get: () => "Win32",
+    });
+  });
+
+  const page = await context.newPage();
+
+  // Function to delay actions
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  const randomDelay = getRandomDelay(2000, 7000);
 
   try {
-    await page.goto("https://jaka.if21pwa.com", {
+    await page.goto(url, {
       waitUntil: "networkidle",
     });
 
+    // Adding a delay to mimic human behavior
+    await delay(randomDelay);
+
     const stringHtml = await page.content();
-    const dom = parse(
-      '<html lang="id">  <head>    <title>Jaka - Teman Jajanmu di Kampus</title>  </head>  <body>    <header class="poppins">      <section></section>      <nav class="middle-nav">        <ul>          <li><a href="">APAAN NIH</a></li>          <li><a href="">DETAIL CUAN</a></li>          <li><a href="">INFO</a></li>        </ul>      </nav>      <nav class="end-nav"></nav>    </header>    <section class="hero">      <section class="title krona">        <h1>JAKA</h1>        <h4>Teman Jajanmu di Kampus</h4>      </section>      <section class="download">        <button>DOWNLOAD NOW</button>      </section>      </section>  </body></html>'
-    );
-    console.log(dom.querySelector(".poppins"));
-    writeFile("scrap.html", stringHtml, (err) => {
-      if (err) {
-        console.error("Error writing to file:", err);
-        return;
-      }
-      console.log("Data has been written to file successfully.");
-    });
+    const dom = parse(stringHtml);
+    const jobList = dom.querySelector('[aria-label="Jobs List"]');
+    if (jobList) {
+      const links = jobList
+        .querySelectorAll("li")
+        .map((link) => link.outerHTML);
+
+      await writeDataToFile(v4(), links.join("\n"));
+    } else {
+      console.error("Jobs List not found.");
+    }
   } catch (error) {
-    console.error("An error occurred:", error.message);
+    console.log("Status: Failed");
+    console.error("Message:", error.message);
   } finally {
     await page.close();
     await browser.close();
+
+    const endTime = new Date(); // Record end time
+    const executionTime = endTime - startTime; // Calculate execution time
+    console.log(`Execution time: ${executionTime / 1000}s`);
   }
 })();
---
+++ readme.md
@@ -0,0 +1,23 @@
+## PREREQUISITES
+
+<ul>
+    <li>NodeJS v18.17.1</li>
+    <li>NPM v9.6.7</li>
+</ul>
+
+## HOW TO USE
+
+```
+npm run scrap <URL>
+
+```
+
+### Example
+
+```
+npm run scrap https://xxxxxxx.xxxxxx/xxxxxx
+```
+
+### FEATURE
+
+1. Get list jobs (html element) then write to file html
